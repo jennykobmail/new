f45c7db55826f30437927207ac98c690ab66c761
jennykobmail <73950108+jennykobmail@users.noreply.github.com>
2020-11-04 22:31:39 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+function calcHeight(iframeElement){
+    var the_height=  iframeElement.contentWindow.document.body.scrollHeight;
+    iframeElement.height=  the_height;
+}
+</script>
+<iframe src="https://www.baby-boomer-retirement.com/2012/08/best-places-to-retire-in-united-states.html" onLoad="calcHeight(this);" frameborder="0" scrolling="no" id="the_iframe" width="100%" ></iframe>
